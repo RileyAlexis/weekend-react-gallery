f3d6e96cc83d8837d58e5abf1d06677103f20552
RileyAlexis <MxRileyAlexis@gmail.com>
2023-08-25 10:55:24 -0500

    Created and applied light theme for material UI

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App.jsx';
+import { ThemeProvider, styled } from '@mui/material/styles';
+import { lightTheme } from './lightTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <ThemeProvider theme={lightTheme}>
         <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
---
+++ src/lightTheme.js
@@ -0,0 +1,39 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+export const lightTheme: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#3f51b5',
+      dark: '#090e27',
+    },
+    secondary: {
+      main: '#8800aa',
+      dark: '#eeadfd',
+    },
+    background: {
+      default: '#bbdefb',
+      paper: '#e0f7fa',
+    },
+  },
+  typography: {
+    fontFamily: 'Source Sans Pro',
+  },
+  spacing: 8,
+  shape: {
+    borderRadius: 4,
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+      },
+    },
+  },
+};
